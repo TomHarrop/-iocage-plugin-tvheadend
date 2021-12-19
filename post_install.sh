7ff85d8edf3f61797d76362018b8ec175207539f
Tom Harrop <twharrop@gmail.com>
2021-12-19 11:37:02 +1100

    disable enable

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+exit 0
+
+
+# tvheadend user will need a login shell for maintenance
+
+# run the -C mode to enable config
+
 # start services
 sysrc tvheadend_enable="YES"
 service tvheadend restart
