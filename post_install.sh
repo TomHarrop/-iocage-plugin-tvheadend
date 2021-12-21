bcb9d0bdd44a9efa91fb9cb4114242b3c3da2401
Tom Harrop <twharrop@gmail.com>
2021-12-22 09:57:24 +1100

    prep ports

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
+# update ports
+portsnap fetch extract
+
 exit 0
 
-
 # tvheadend user will need a login shell for maintenance
+
+
+# download tvheadend
+git clone https://github.com/tvheadend/tvheadend.git \
+	/tvh-build
+
+cd /tvh-build || exit 1
+
+# bash path is hardcoded
+sed -i'configure.bak' \
+	's|/bin/bash|/usr/local/bin/bash|g' configure
+
+
+
+# build owntone
+./configure \
+	--disable-dvbscan \
+	--disable-linuxdvb \
+	--disable-zlib \
+	--disable-hdhomerun_static \
+	--disable-bintray_cache \
+	--disable-ffmpeg_static \
+	--disable-pie
+
 
 # run the -C mode to enable config
 
